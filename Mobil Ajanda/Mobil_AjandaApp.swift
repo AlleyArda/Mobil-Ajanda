f396365065b4a49423f15a7ee30517424522e058
Ali Arda Kulaksız <92370387+AlleyArda@users.noreply.github.com>
2024-07-05 10:23:56 +0300

    Initial Commit

---
+++ Mobil Ajanda/Mobil_AjandaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mobil_AjandaApp.swift
+//  Mobil Ajanda
+//
+//  Created by Arda Kulaksız on 5.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Mobil_AjandaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
