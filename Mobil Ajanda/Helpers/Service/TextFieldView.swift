4bab2ae7c69ec503603ab327d0f43bf9cdd109f0
Ali Arda Kulaksız <92370387+AlleyArda@users.noreply.github.com>
2024-07-09 15:01:35 +0300

    full auth eklendi giriş ekranı tamamen yapıldı

---
+++ Mobil Ajanda/Helpers/Service/TextFieldView.swift
@@ -0,0 +1,18 @@
+//
+//  TextFieldView.swift
+//  Mobil Ajanda
+//
+//  Created by Arda Kulaksız on 8.07.2024.
+//
+
+import SwiftUI
+
+struct TextFieldView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextFieldView()
+}
