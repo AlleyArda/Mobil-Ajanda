93dfbcb08069f74c62417f1f53f568f2003ef0b8
Ali Arda Kulaksız <92370387+AlleyArda@users.noreply.github.com>
2024-07-09 16:00:26 +0300

    logo eklendi, headersview güncellendi

--- Mobil Ajanda/App/Auth/View/SharedView/HeaderView.swift
+++ Mobil Ajanda/App/Auth/View/SharedView/HeaderView.swift
@@ -8,8 +8,9 @@ struct HeaderView: View {
     
     var body: some View {
         ZStack {
-            RoundedRectangle(cornerRadius: 0)
+            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
                 .foregroundStyle(background.gradient)
+                .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/.opacity(0.3), radius: 20 , x: 0 , y: 10)
 
             
             VStack {
@@ -17,12 +18,23 @@ struct HeaderView: View {
                     .font(.system(size: 40))
                     .foregroundStyle(.white)
                     .bold()
-                
+                    
                 Text(subTitle)
                     .font(.system(size: 20))
                     .italic()
                     .bold()
                     .foregroundStyle(.white)
+                    
+                
+                Image("tedas-logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/ , height: 100)
+                    .padding()
+                    .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/.opacity(0.3), radius: 20 , x: 0 , y: 10)
+                    
+                    
+                    
             }
 
         }
