4236729ea856163e3cc603afdb81f17c57d6b7b5
Ali Arda Kulaksız <92370387+AlleyArda@users.noreply.github.com>
2024-07-28 00:45:57 +0300

    AuthView animations

--- Mobil Ajanda/App/Auth/View/SharedView/HseaderView.swift
+++ Mobil Ajanda/App/Auth/View/SharedView/HseaderView.swift
@@ -1,44 +1,58 @@
 import SwiftUI
-//jul 9 15.06
-struct HeaderView: View {
-    let title : String
-    let subTitle : String
 
+struct HeaderView: View {
+    let title: String
+    let subTitle: String
+    @State private var titleOpacity: Double = 0.0
+    @State private var subTitleOpacity: Double = 0.0
+    @State private var logoAnimation = false
     let background: Color
-    
+
     var body: some View {
         ZStack {
-            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+            RoundedRectangle(cornerRadius: 25.0)
                 .foregroundStyle(background.gradient)
-                .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/.opacity(0.3), radius: 20 , x: 0 , y: 10)
+                .shadow(color: .black.opacity(0.3), radius: 20, x: 0, y: 10)
 
-            
             VStack {
                 Text(title)
                     .font(.system(size: 40))
                     .foregroundStyle(.white)
                     .bold()
-                    
+                    .opacity(titleOpacity)
+                    .onAppear {
+                        withAnimation(Animation.easeOut(duration: 2.0)) {
+                            self.titleOpacity = 1.0
+                        }
+                    }
+
                 Text(subTitle)
                     .font(.system(size: 20))
                     .italic()
                     .bold()
                     .foregroundStyle(.white)
-                    
-                
+                    .opacity(subTitleOpacity)
+                    .onAppear {
+                        withAnimation(Animation.easeOut(duration: 2.0).delay(1.0)) {
+                            self.subTitleOpacity = 1.0
+                        }
+                    }
+
                 Image("tedas-logo")
                     .resizable()
                     .scaledToFit()
-                    .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/ , height: 100)
+                    .frame(width: 100, height: 100)
                     .padding()
-                    .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/.opacity(0.3), radius: 20 , x: 0 , y: 10)
-                    
-                    
-                    
+                    .shadow(color: .black.opacity(0.3), radius: 20, x: 0, y: 10)
+                    .scaleEffect(logoAnimation ? 1.0 : 1.1)
+                    .opacity(logoAnimation ? 0.7 : 1.0)
+                    .onAppear {
+                        withAnimation(Animation.easeInOut(duration: 2.0).repeatForever(autoreverses: true).delay(2.0)) {
+                            self.logoAnimation = true
+                        }
+                    }
             }
-
         }
-        
     }
 }
 
